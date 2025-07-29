3d1d3465e735ce38625f9506142b162b88177b98
ariel <ariel10player@gmail.com>
2025-07-29 06:56:24 -0300

    save lives is LS (BUGGED)

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from 'react'
 import './App.css'
 
@@ -17,7 +16,12 @@ export const App = () => {
   const [loading, setLoading] = useState<boolean>(true)
   const [mensajeFinal, setMensajeFinal] = useState<string>()
   const totalVidas = 5
-  const vidasRestantes = totalVidas - intentos.length
+  const [vidasRestantes, setVidasRestantes] = useState<number>(() => {
+    const vidasLS = localStorage.getItem("vidasRestantes");
+    return vidasLS !== null ? Number(vidasLS) : totalVidas;
+  });
+  const [modoOscuro, setModoOscuro] = useState(false);
+  const [cartelVidas, setCartelVidas] = useState<boolean>(false)
 
 
   const updateWord = async (respuesta: string) => {
@@ -40,6 +44,7 @@ export const App = () => {
     }
 
   }
+
   const forCantLetras = (parametro: string) => {
     const arrayAux = []
     for (let i = 0; i < parametro.length; i++) {
@@ -47,18 +52,21 @@ export const App = () => {
     }
     setCantLetras(arrayAux)
   }
-  const vaciarLocalStorage = () => {
-    if (vidasRestantes != 0) {
 
+  const vaciarLocalStorage = () => {
+    if (vidasRestantes != 0 && finJuego) {
       localStorage.removeItem("resuelto")
       localStorage.removeItem("respuesta")
       localStorage.removeItem("respuestaCorrecta")
+      setIntentos([])
       setMensajeFinal("")
-      window.location.reload()
+      setFinJuego(false)
+      fetchData()
     } else {
       console.log("te quedaste sin vidas")
     }
   }
+
   const quitarAcentos = (letra: string) => {
     return letra.replace(/[áÁ]/g, 'A')
       .replace(/[éÉ]/g, 'E')
@@ -66,44 +74,39 @@ export const App = () => {
       .replace(/[óÓ]/g, 'O')
       .replace(/[úÚ]/g, 'U')
   }
+
   const corazonesAnimacion = () => {
     if (vidasRestantes <= 2) {
       return "animate-girar"
     }
-
   }
-  //FETCH DE LA PALABRA
-  useEffect(() => {
 
+  const fetchData = async () => {
     const resuelto = localStorage.getItem("resuelto")
     const intentosLS = localStorage.getItem("respuesta")
     if (!resuelto && !intentosLS && vidasRestantes !== 0) {
-      const fetchData = async () => {
-        setLoading(true)
-        try {
-          const res = await fetch("http://localhost:3000/word")
-          const data = await res.json()
-          if (!res.ok) {
-            throw new Error("Error al traer productos"), await data.message;
-          }
-          setRespuesta(quitarAcentos(data.name.toUpperCase()))
-          localStorage.setItem("respuestaCorrecta", quitarAcentos(data.name.toUpperCase()))
-          forCantLetras(data.name)
-          setLoading(false)
-        } catch (error) {
-          console.log("Error al traer palabra", error)
-
+      setLoading(true)
+      try {
+        const res = await fetch("http://localhost:3000/word")
+        const data = await res.json()
+        if (!res.ok) {
+          throw new Error("Error al traer productos"), await data.message;
         }
+        setRespuesta(quitarAcentos(data.name.toUpperCase()))
+        localStorage.setItem("respuestaCorrecta", quitarAcentos(data.name.toUpperCase()))
+        forCantLetras(data.name)
+        setLoading(false)
+      } catch (error) {
+        console.log("Error al traer palabra", error)
+
       }
-      fetchData()
     }
-  }, [])
+  }
 
-
-  // CONSOLE LOG PARA CONTROLAR 
+  //FETCH DE LA PALABRA
   useEffect(() => {
-    console.log(respuesta)
-  }, [respuesta])
+    fetchData()
+  }, [])
 
 
   //Ingreso Letras
@@ -147,9 +150,9 @@ export const App = () => {
     const checkResults = (e: KeyboardEvent) => {
       if (e.key === "Enter" && letras.length == respuesta.length && !finJuego) {
         const nuevosColores: string[] = letras.map((l, i) => {
-          if (l === respuesta[i]) return `bg-green-400 animate-pulse  hover:-translate-y-20`
-          if (l !== respuesta[i] && respuesta.includes(l)) return "bg-yellow-400"
-          return "bg-red-400"
+          if (l === respuesta[i]) return `bg-green-400 animate-pulse  hover:-translate-y-20 dark:bg-green-800`
+          if (l !== respuesta[i] && respuesta.includes(l)) return "bg-yellow-400 dark:bg-yellow-600"
+          return "bg-red-400 dark:bg-red-800"
         })
 
         setIntentos(prev => [
@@ -165,6 +168,10 @@ export const App = () => {
           setFinJuego(true)
           setCantLetras([])
           setMensajeFinal("ganaste")
+        } else {
+          const vidasDiferencia = vidasRestantes - 1;
+          setVidasRestantes(vidasDiferencia);
+          localStorage.setItem("vidasRestantes", JSON.stringify(vidasDiferencia));
         }
         setLetras([])
         forCantLetras(respuesta)
@@ -175,53 +182,66 @@ export const App = () => {
     return () => {
       window.removeEventListener("keydown", checkResults)
     }
-  }, [letras, respuesta, intentos.length, finJuego])
+  }, [letras, respuesta, intentos, finJuego])
 
 
-  //Agregar intentos al LS
+  //Control de intentos y vidas en el LocalStorage
   useEffect(() => {
+    const resuelto = localStorage.getItem("resuelto")
+
+    const vidasDiferencia = vidasRestantes
+    localStorage.setItem("vidasRestantes", JSON.stringify(vidasDiferencia))
+    const vidasRestantesLS = localStorage.getItem("vidasRestantes")
+
+    if (vidasRestantesLS && vidasRestantes == undefined) {
+      setVidasRestantes(Number(vidasRestantesLS))
+    }
+
     if (intentos.length != 0) {
       localStorage.setItem("respuesta", JSON.stringify(intentos))
     }
 
-  }, [intentos])
-
-
-  //Fin del juego si hay 5 intentos
-  useEffect(() => {
     if (intentos.length >= 5) {
       setCantLetras([]);
       setFinJuego(true)
     }
 
-    const resuelto = localStorage.getItem("resuelto")
-    if (intentos.length >= 5 && !resuelto) {
+    if (finJuego && !resuelto) {
       setMensajeFinal("perdiste")
     }
-    if (resuelto) {
+
+    if (resuelto && finJuego) {
       setCantLetras([]);
       setMensajeFinal("ganaste")
+      setFinJuego(true)
     }
-  }, [intentos.length]);
+
+  }, [intentos, finJuego, vidasRestantes]);
 
 
-  //CHECK localStorage
+  //Traer datos del localStorage
   useEffect(() => {
-    setLoading(true)
+    const respuestaCorrectaLS = localStorage.getItem("respuestaCorrecta")
+    const vidasRestantesLS = localStorage.getItem("vidasRestantes")
     const respuestaStorage = localStorage.getItem("resuelto")
+    const intentosLS = localStorage.getItem("respuesta")
+    setLoading(true)
+
     if (respuestaStorage) {
       setCantLetras([])
       setFinJuego(true)
       setMensajeFinal("ganaste")
     }
 
-    const intentosLS = localStorage.getItem("respuesta")
-    const respuestaCorrectaLS = localStorage.getItem("respuestaCorrecta")
     if (intentosLS && respuestaCorrectaLS) {
       setIntentos(JSON.parse(intentosLS))
       setRespuesta(respuestaCorrectaLS)
       forCantLetras(respuestaCorrectaLS)
       setLoading(false)
+    }
+
+    if (vidasRestantes == undefined && !vidasRestantesLS) {
+      setVidasRestantes(totalVidas)
     }
     const timer = setTimeout(() => {
       setAnimar(true)
@@ -230,10 +250,32 @@ export const App = () => {
   }, [])
 
 
+  //Escuchador del modo oscuro god
+  useEffect(() => {
+    const matchDark = window.matchMedia('(prefers-color-scheme: dark)');
+    const handleChange = (e: MediaQueryListEvent) => {
+      setModoOscuro(e.matches);
+    };
+    setModoOscuro(matchDark.matches); // Set inicial
+    matchDark.addEventListener('change', handleChange); // Detectar cambios
+    return () => matchDark.removeEventListener('change', handleChange);
+  }, []);
+
+
+  // CONSOLE LOG PARA CONTROLAR 
+  // useEffect(() => {
+  //   if (vidasRestantes) {
+
+  //     console.log(vidasRestantes)
+  //   }
+
+  // }, [vidasRestantes])
+
+
 
   return (
 
-    <>
+    <main className={modoOscuro ? "bg-black text-white h-full " : "bg-white text-black h-full"}>
       <h1 className={` font-press justify-self-center text-3xl p-10 transition-all ease-in-out delay-75 duration-750 transform ${animar ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`}>Wordle </h1>
 
       {loading ? (
@@ -252,7 +294,7 @@ export const App = () => {
               </div>
             ))}
           </div>
-          <aside className='bg-amber-200 w-1/6 top-0 right-40 border-2 flex absolute text-black flex-col items-center rounded-md'>
+          <aside className='bg-amber-200 dark:bg-amber-400 w-1/6 top-0 right-40 border-2 flex absolute text-black flex-col items-center rounded-md'>
             <span className="font-press block text-xl mb-2 w-auto ">Vidas:</span>
             <div className={`flex gap-2 text-2xl `}>
               {Array.from({ length: totalVidas }, (_, i) => (
@@ -273,13 +315,13 @@ export const App = () => {
       <div className={`flex gap-2  justify-center mb-5 transition-all delay-150 duration-750 ease-in-out ${animar ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"} `}>
 
         {letras.map((letra, i) => (
-          <div key={i} className={` w-[60px] h-[60px] border-2 border-red-400 flex items-center justify-center  text-2xl font-bold uppercase transition-all ease-in-out delay-150 duration-150 rounded-sm  `}>
+          <div key={i} className={` dark:border-gray-200 w-[60px] h-[60px] border-2  border-red-400 flex items-center justify-center  text-2xl font-bold uppercase transition-all ease-in-out delay-150 duration-150 rounded-sm  `}>
             {letra}
           </div>
         ))}
 
         {cantLetras.map((l) => (
-          <div key={l} className={`w-[60px] h-[60px] border-2 border-red-700 flex items-center justify-center text-2xl font-bold uppercase bg-white transition-all ease-in-out delay-75 duration-700 transform rounded-sm `}>
+          <div key={l} className={`w-[60px] h-[60px] border-2 border-red-700 flex items-center justify-center text-2xl font-bold uppercase bg-white dark:bg-gray-600 dark:border-gray-200 transition-all ease-in-out delay-75 duration-700 transform rounded-sm `}>
           </div>
         )
         )}
@@ -288,26 +330,26 @@ export const App = () => {
       <div className='flex justify-center mb-3 animate-girar '>
 
         {mensajeFinal?.split("").map((l, i) =>
-          <div key={i} className={`w-[40px] h-[40px]  cursor-pointer delay-300  text-white ${mensajeFinal == "perdiste" ? "bg-orange-400 animate-wiggle hover:animate-none" : "bg-blue-400 animate-girar hover:animate-none"}  border-2 border-white flex items-center justify-center text-2xl font-bold uppercase rounded-sm transition-all ease-in-out  duration-300  hover:translate-y-1  }`}>
+          <div key={i} className={`w-[40px] h-[40px]   cursor-pointer delay-300  text-white ${mensajeFinal == "perdiste" ? "bg-orange-400 animate-wiggle hover:animate-none dark:bg-orange-800" : "bg-blue-400 animate-girar hover:animate-none"}  border-2 dark:border-gray-800 border-white flex items-center justify-center text-2xl font-bold uppercase rounded-sm transition-all ease-in-out  duration-300  hover:translate-y-1  }`}>
             {l}
           </div>)}
       </div>
 
       {finJuego &&
-        <div className='flex relative  justify-self-center border-2 font-bold border-black p-2 rounded-md bg-blue-400 text-white  cursor-pointer transition-all text-2xl ease-in-out duration-300 hover:bg-white hover:text-black hover:translate-y-1 hover:animate-none'>
-          <button className='cursor-pointer' onClick={vaciarLocalStorage}>Jugar de nuevo </button>
+        <div className='flex relative dark:shadow-white dark:shadow-xl/20 justify-self-center border-2 font-bold border-black p-2 rounded-md bg-blue-400 text-white  cursor-pointer transition-all text-2xl ease-in-out duration-300 hover:bg-white hover:text-black hover:translate-y-1 hover:animate-none'>
+          <button className='cursor-pointer' onClick={() => [vaciarLocalStorage(), setCartelVidas(vidasRestantes == 0 ? true : false)]}>Jugar de nuevo </button>
         </div>
       }
-      {vidasRestantes == 0 &&
-        <div className='flex justify-center bg-amber-200 absolute rounded-md border-2 top-1/3 left-1/2 -translate-x-1/2 w-1/3 h-20 items-center '>
-          <span>Te quedaste sin vidas</span> 
-          <p>Intentalo nuevamente mas tarde</p>
-          <span className='absolute top-[-2px] right-0 text-2xl bg-white rounded-b-2xl border-2 cursor-pointer pb-2 pr-2 pl-2'>x</span>
+      {cartelVidas &&
+        <div className='flex gap-1 justify-center  dark:text-black text-md font-press bg-amber-200 absolute rounded-md border-2 top-1/3 left-1/2 -translate-x-1/2 w-1/3 h-20 items-center '>
+          <span>Te quedaste sin vidas</span>
+          <p>Inténtalo nuevamente mas tarde</p>
+          <span className='absolute top-[-2px] right-0 text-2xl bg-white hover:-translate-y-1 rounded-b-2xl border-2 cursor-pointer pb-2 pr-2 pl-2' onClick={() => setCartelVidas(!cartelVidas)}>x</span>
         </div>
       }
 
 
-      <footer className='font-press font-extralight w-full sticky-b-0  absolute bottom-0 justify-center flex'>
+      <footer className='font-press font-extralight w-full sticky-b-0 absolute bottom-0 justify-center flex'>
         <div className='w-1/2 justify-evenly flex-row '>
           <h2 className='text-2xl justify-self-center'>Aclaraciones:</h2>
           <ul className='flex gap-[10px] justify-center'>
@@ -326,7 +368,8 @@ export const App = () => {
           </ul>
         </div>
       </footer>
-    </>
+    </main>
+
   )
 }
 
