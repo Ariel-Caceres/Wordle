74aba89c198b31af21c860ffc920751c9bc55145
ariel <ariel10player@gmail.com>
2025-08-02 00:26:34 -0300

    chore: backend enpoints

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -8,7 +8,51 @@ const cors = require("cors")
 app.use(express.json())
 app.use(cors())
 
-app.get("/word", async (req, res) => {
+app.get("/word/spanish", async (req, res) => {
+    const palabra = await db.word.findOne(
+        {
+            where: { isCompleted: false, language_id: 1 },
+            order: Sequelize.literal("RAND()")
+        })
+    if (!palabra) return json.status(400).json("Error al encontrar palabra")
+    res.json(palabra)
+})
+
+app.get("/word/spanish/:dificulty", async (req, res) => {
+    let dificulty = parseInt(req.params.dificulty)
+
+    const palabra = await db.word.findOne(
+        {
+            where: { isCompleted: false, language_id: 1, difficulty_id: { dificulty } },
+            order: Sequelize.literal("RAND()")
+        })
+    if (!palabra) return json.status(400).json("Error al encontrar palabra")
+    res.json(palabra)
+})
+
+app.get("/word/english", async (req, res) => {
+
+    const palabra = await db.word.findOne(
+        {
+            where: { isCompleted: false, language: 2 },
+            order: Sequelize.literal("RAND()")
+        })
+    if (!palabra) return json.status(400).json("Error al encontrar palabra")
+    res.json(palabra)
+})
+
+app.get("/word/english/:difficulty", async (req, res) => {
+    let difficulty = parseInt(req.params.difficulty)
+    const palabra = await db.word.findOne(
+        {
+            where: { isCompleted: false, language_id: { difficulty } },
+            order: Sequelize.literal("RAND()")
+        })
+    if (!palabra) return json.status(400).json("Error al encontrar palabra")
+    res.json(palabra)
+})
+
+app.get("/word/randomLanguage", async (req, res) => {
     const palabra = await db.word.findOne(
         {
             where: { isCompleted: false },
@@ -25,6 +69,7 @@ app.put("/done", async (req, res) => {
     await palabraEncontrada.update({ isCompleted: true })
     res.json("todo ok")
 })
+
 app.listen(port, async () => {
     // await db.sequelize.sync({ force: true });
     console.log(`App listening on  http://localhost:${port}`);
